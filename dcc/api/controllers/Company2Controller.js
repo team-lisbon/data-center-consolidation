8411cd7e7f55aea8732582f5600311671d784c2d
jnguyenq <jnguyenq@gmu.edu>
2015-10-16 12:41:08 -0400

    wip

---
+++ dcc/api/controllers/Company2Controller.js
@@ -0,0 +1,11 @@
+/**
+ * Company2Controller
+ *
+ * @description :: Server-side logic for managing company2s
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
